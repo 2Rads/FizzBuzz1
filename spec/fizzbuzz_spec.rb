459ba1b5d3eb7b3a10176ca52e2ce6587b1414cc
ranjot <ranjotsd@gmail.com>
2021-09-14 13:43:30 +0100

    Added another test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,10 @@
+require './spec/lib/fizzbuzz'
 describe  'fizzbuzz' do 
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-end
 
+  it 'returns "buzz" for the number 5' do
+    expect(fizzbuzz(5))).to eq 'buzz'
+  end
+end
---
+++ spec/lib/fizzbuzz.rb
@@ -0,0 +1,8 @@
+def fizzbuzz(number)
+  if number == 3
+    return 'fizz'
+  elsif number == 5 
+    return 'buzz'
+  end
+end
+
